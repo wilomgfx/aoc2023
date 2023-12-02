e762b98f7d01dacb170cfbc673a032599adb40ea
William Cantin <wzc01@acuitysso.com>
2023-12-02 13:05:06 -0500

    Day2 done

--- aoc2023/src/days/day2/day.test.ts
+++ aoc2023/src/days/day2/day.test.ts
@@ -4,34 +4,35 @@ import input from "./input";
 describe("Day2", () => {
   describe("Part1", () => {
     it("should return the correct answer for the example provided", () => {
-      const input = `1abc2
-      pqr3stu8vwx
-      a1b2c3d4e5f
-      treb7uchet
+      const input = `
+      Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+      Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+      Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+      Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+      Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
       `;
-      const expected = 142;
+      const expected = 8;
       expect(part1(input)).toBe(expected);
     });
     it("should return the correct answer for part 1", () => {
-      expect(part1(input)).toBe(53194);
+      expect(part1(input)).toBe(2377);
     });
   });
 
   describe("Part2", () => {
     it("should return the correct answer for the example provided", () => {
-      const input = `two1nine
-      eightwothree
-      abcone2threexyz
-      xtwone3four
-      4nineeightseven2
-      zoneight234
-      7pqrstsixteen
+      const input = `
+      Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+      Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+      Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+      Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+      Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
       `;
-      const expected = 281;
+      const expected = 2286;
       expect(part2(input)).toBe(expected);
     });
     it("should return the correct answer for part 2", () => {
-      expect(part2(input)).toBe(54249);
+      expect(part2(input)).toBe(71220);
     });
   });
 });
--- aoc2023/src/days/day2/day.ts
+++ aoc2023/src/days/day2/day.ts
@@ -4,7 +4,39 @@ const part1 = (input: string) => {
     .map((i) => i.trim())
     .filter((i) => i !== "");
 
-  const answer = "N/A";
+  const NUMBER_COLOR_REGEX = /(\d+)\s(\w+)/g;
+  const MAX_RED_CUBES = 12;
+  const MAX_GREEN_CUBES = 13;
+  const MAX_BLUE_CUBES = 14;
+  const validGames = inputs.filter((g) => {
+    const gameWithoutId = g.split(":")[1].trim();
+    let match;
+    const result = {} as Record<string, number[]>;
+
+    while ((match = NUMBER_COLOR_REGEX.exec(gameWithoutId)) !== null) {
+      const number = parseInt(match[1]);
+      const color = match[2];
+
+      if (result[color]) {
+        result[color].push(number);
+      } else {
+        result[color] = [number];
+      }
+    }
+    return (
+      result["red"].every((r) => r <= MAX_RED_CUBES) &&
+      result["green"].every((r) => r <= MAX_GREEN_CUBES) &&
+      result["blue"].every((r) => r <= MAX_BLUE_CUBES)
+    );
+  });
+  const validGamesIds = validGames.map((g) => {
+    const id = g.split(":")[0].trim();
+    const gameIdRegex = /Game\s(\d+)/;
+    const match = id.match(gameIdRegex) || []; //should not happen, but heh :)
+    return match[1];
+  });
+  const gameIdsSum = validGamesIds.reduce((a, b) => a + parseInt(b), 0);
+  const answer = gameIdsSum;
   return answer;
 };
 
@@ -14,7 +46,39 @@ const part2 = (input: string) => {
     .map((i) => i.trim())
     .filter((i) => i !== "");
 
-  const answer = "N/A";
+  const NUMBER_COLOR_REGEX = /(\d+)\s(\w+)/g;
+  // const MAX_RED_CUBES = 12;
+  // const MAX_GREEN_CUBES = 13;
+  // const MAX_BLUE_CUBES = 14;
+  const validGames = inputs.map((g) => {
+    const gameId = g.split(":")[0].trim();
+    const gameWithoutId = g.split(":")[1].trim();
+    let match;
+    const result = {} as Record<string, number[]>;
+
+    while ((match = NUMBER_COLOR_REGEX.exec(gameWithoutId)) !== null) {
+      const number = parseInt(match[1]);
+      const color = match[2];
+
+      if (result[color]) {
+        result[color].push(number);
+      } else {
+        result[color] = [number];
+      }
+    }
+    return {
+      id: gameId,
+      red: Math.max(...result["red"]),
+      green: Math.max(...result["green"]),
+      blue: Math.max(...result["blue"]),
+    };
+  });
+  const gamePowers = validGames.map((g) => {
+    const power = g.red * g.green * g.blue;
+    return power;
+  });
+  const powerSum = gamePowers.reduce((a, b) => a + b, 0);
+  const answer = powerSum;
   return answer;
 };
 

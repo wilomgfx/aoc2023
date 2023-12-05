c55c9007ce788dbab0390a917eca83ca3f3b6420
William Cantin <wzc01@acuitysso.com>
2023-12-05 11:47:57 -0500

    WIP day 5

--- aoc2023/src/days/day5/day.ts
+++ aoc2023/src/days/day5/day.ts
@@ -1,8 +1,33 @@
+type MapObject = {
+  [key: string]: { [key: number]: number[] };
+};
+
+const parseArray = (arr: string[]): MapObject => {
+  let mapName = "";
+  const result: MapObject = {};
+
+  for (const [index, line] of arr.entries()) {
+    if (line.includes("map")) {
+      mapName = line.replace(" map:", "");
+
+      result[mapName] = {};
+    } else {
+      const [key, ...values] = line.split(" ").map(Number);
+      if (!isNaN(key)) {
+        result[mapName][index] = [key, ...values];
+      }
+    }
+  }
+
+  return result;
+};
 const part1 = (input: string) => {
   const inputs = input
     .split("\n")
     .map((i) => i.trim())
     .filter((i) => i !== "");
+
+  const mappedInputs = parseArray(inputs);
 
   const DESTINATION_RANGE_START_INDEX = 0;
   const SOURCE_RANGE_START_INDEX = 1;
